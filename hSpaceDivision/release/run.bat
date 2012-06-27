9fdd9706c2217d476f20dd7622257121665740cc
waytofall916@gmail.com <waytofall916@gmail.com@d03a6f8b-bca6-27b0-1c56-6a6834aa119e>
2012-06-27 03:47:27 +0000

    邻接信息检查改成广度优先，解决栈溢出的问题

--- hSpaceDivision/release/run.bat
+++ hSpaceDivision/release/run.bat
@@ -3,5 +3,5 @@
 ::F:\plys\happy_recon\happy_vrip.ply
 ::d:/dragon_recon/dragon_vrip.ply
 ::d:/bun_zipper.ply
-hSpaceDivision.exe -t 4000 F:\plys\happy_recon\happy_vrip.ply
+hSpaceDivision.exe -t 8000 F:\plys\dragon_recon\dragon_vrip.ply
 pause 
